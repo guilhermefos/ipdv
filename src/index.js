a3b0f0b822e1e894e05b38a3f62fa828ae9b3d00
Guilherme Silva <guilhermefos.developer@gmail.com>
2018-09-28 13:11:01 -0300

    changes to deploy

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import routes from './routes';
 
-ReactDOM.render(<App routes={routes} />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
